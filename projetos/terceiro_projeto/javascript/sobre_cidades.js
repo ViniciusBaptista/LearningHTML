11577547acc092dc2b278ac2dc7f55a939d1da08
ViniciusBaptista <vbaptista7@gmail.com>
2014-08-17 20:55:00 -0300

    Adicionando JavaScript e jQuery

---
+++ projetos/terceiro_projeto/javascript/sobre_cidades.js
@@ -0,0 +1,15 @@
+$(function() {
+  $('.article').click(function() {
+    $('.article').removeClass('current');
+    $('.description').hide();
+    
+    $(this).addClass('current');
+    $(this).children('.description').show();
+  });
+
+  $(document).keypress(function(event) {
+    if (event.which === 111) {
+      $('.current').children('.description').toggle();
+    }
+  });
+});
